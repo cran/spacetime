cac2e317f95fe0e16c8e94a24510cf8235e3f0dc
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2023-04-05 19:10:02 +0000

    version 1.3-0

--- inst/doc/jss816.R
+++ inst/doc/jss816.R
@@ -10,8 +10,8 @@ set.seed(1331)
 ###################################################
 ### code chunk number 2: jss816.Rnw:185-187
 ###################################################
-if (require(foreign, quietly = TRUE) && require(maptools, quietly = TRUE))
-  read.dbf(system.file("shapes/sids.dbf", package="maptools"))[1:5,c(5,9:14)]
+if (require(foreign, quietly = TRUE) && require(sf, quietly = TRUE))
+  read.dbf(system.file("shape/nc.dbf", package="sf"))[1:5,c(5,9:14)]
 
 
 ###################################################
@@ -111,7 +111,7 @@ par(opar)
 
 
 ###################################################
-### code chunk number 6: jss816.Rnw:586-590
+### code chunk number 6: jss816.Rnw:585-589
 ###################################################
 sp = cbind(x = c(0,0,1), y = c(0,1,1))
 row.names(sp) = paste("point", 1:nrow(sp), sep="")
@@ -120,13 +120,13 @@ sp = SpatialPoints(sp)
 
 
 ###################################################
-### code chunk number 7: jss816.Rnw:594-595
+### code chunk number 7: jss816.Rnw:593-594
 ###################################################
 time = as.POSIXct("2010-08-05", tz = "GMT")+3600*(10:13)
 
 
 ###################################################
-### code chunk number 8: jss816.Rnw:598-602
+### code chunk number 8: jss816.Rnw:597-601
 ###################################################
 m = c(10,20,30) # means for each of the 3 point locations
 values = rnorm(length(sp)*length(time), mean = rep(m, 4))
@@ -135,38 +135,38 @@ mydata = data.frame(values = signif(values, 3), ID=IDs)
 
 
 ###################################################
-### code chunk number 9: jss816.Rnw:605-606
+### code chunk number 9: jss816.Rnw:604-605
 ###################################################
 library(spacetime)
 
 
 ###################################################
-### code chunk number 10: jss816.Rnw:608-610 (eval = FALSE)
+### code chunk number 10: jss816.Rnw:607-609 (eval = FALSE)
 ###################################################
 ## library(spacetime)
 ## stfdf = STFDF(sp, time, data = mydata)
 
 
 ###################################################
-### code chunk number 11: jss816.Rnw:615-616
+### code chunk number 11: jss816.Rnw:614-615
 ###################################################
 stfdf = STFDF(sp, time, mydata, time+60)
 
 
 ###################################################
-### code chunk number 12: jss816.Rnw:667-668 (eval = FALSE)
+### code chunk number 12: jss816.Rnw:666-667 (eval = FALSE)
 ###################################################
 ## air_quality[2:3, 1:10, "PM10"]
 
 
 ###################################################
-### code chunk number 13: jss816.Rnw:672-673 (eval = FALSE)
+### code chunk number 13: jss816.Rnw:671-672 (eval = FALSE)
 ###################################################
 ## air_quality[Germany, "2008::2009", "PM10"]
 
 
 ###################################################
-### code chunk number 14: jss816.Rnw:692-695
+### code chunk number 14: jss816.Rnw:691-694
 ###################################################
 xs1 = as(stfdf, "Spatial")
 class(xs1)
@@ -174,13 +174,13 @@ xs1
 
 
 ###################################################
-### code chunk number 15: jss816.Rnw:699-700
+### code chunk number 15: jss816.Rnw:698-699
 ###################################################
 attr(xs1, "time")
 
 
 ###################################################
-### code chunk number 16: jss816.Rnw:704-708
+### code chunk number 16: jss816.Rnw:703-707
 ###################################################
 x = as(stfdf, "STIDF")
 xs2 = as(x, "Spatial")
@@ -189,14 +189,14 @@ xs2[1:4,]
 
 
 ###################################################
-### code chunk number 17: jss816.Rnw:755-757 (eval = FALSE)
+### code chunk number 17: jss816.Rnw:754-756 (eval = FALSE)
 ###################################################
 ## scales=list(x=list(rot = 45))
 ## stplot(wind.data, mode = "xt", scales = scales, xlab = NULL)
 
 
 ###################################################
-### code chunk number 18: jss816.Rnw:805-816 (eval = FALSE)
+### code chunk number 18: jss816.Rnw:804-815 (eval = FALSE)
 ###################################################
 ## # code to create figure 5.
 ## library(lattice)
@@ -212,73 +212,69 @@ xs2[1:4,]
 
 
 ###################################################
-### code chunk number 19: jss816.Rnw:858-860
+### code chunk number 19: jss816.Rnw:857-859
 ###################################################
 library(xts)
 .parseISO8601('2010-05')
 
 
 ###################################################
-### code chunk number 20: jss816.Rnw:865-866
+### code chunk number 20: jss816.Rnw:864-865
 ###################################################
 .parseISO8601('2010-05-01T13:30/2010-05-01T13:39')
 
 
 ###################################################
-### code chunk number 21: jss816.Rnw:937-941
+### code chunk number 21: jss816.Rnw:936-940
 ###################################################
-if (require(maptools, quietly = TRUE) && require(rgdal, quietly = TRUE)) {
-  fname = system.file("shapes/sids.shp", package="maptools")[1]
-  nc = rgdal::readOGR(fname)
+if (require(sf, quietly = TRUE)) {
+  fname = system.file("gpkg/nc.gpkg", package = "sf")[1]
+  nc = as(sf::st_read(fname), "Spatial")
 }
 
 
 ###################################################
-### code chunk number 22: jss816.Rnw:944-946
+### code chunk number 22: jss816.Rnw:943-945
 ###################################################
 time = as.POSIXct(c("1974-07-01", "1979-07-01"), tz = "GMT")
 endTime = as.POSIXct(c("1978-06-30", "1984-06-30"), tz = "GMT")
 
 
 ###################################################
-### code chunk number 23: jss816.Rnw:949-955
+### code chunk number 23: jss816.Rnw:948-952
 ###################################################
-if (require(maptools, quietly = TRUE) && require(rgdal, quietly = TRUE)) {
 data = data.frame(
 	BIR = c(nc$BIR74, nc$BIR79),
 	NWBIR = c(nc$NWBIR74, nc$NWBIR79),
 	SID = c(nc$SID74, nc$SID79))
-}
 
 
 ###################################################
-### code chunk number 24: jss816.Rnw:958-961
+### code chunk number 24: jss816.Rnw:955-956
 ###################################################
-if (require(maptools, quietly = TRUE) && require(rgdal, quietly = TRUE)) {
- nct = STFDF(sp = as(nc, "SpatialPolygons"), time, data, endTime)
-}
+nct = STFDF(sp = as(nc, "SpatialPolygons"), time, data, endTime)
 
 
 ###################################################
-### code chunk number 25: jss816.Rnw:970-976
+### code chunk number 25: jss816.Rnw:965-971
 ###################################################
 if (require(maps, quietly = TRUE)) {
  states.m <- map('state', plot=FALSE, fill=TRUE)
  IDs <- sapply(strsplit(states.m$names, ":"), function(x) x[1])
 }
-if (require(maptools, quietly = TRUE))
-  states <- map2SpatialPolygons(states.m, IDs=IDs)
+if (require(sf, quietly = TRUE))
+  states <- as(st_geometry(st_as_sf(states.m, IDs=IDs)), "Spatial")
 
 
 ###################################################
-### code chunk number 26: jss816.Rnw:979-981
+### code chunk number 26: jss816.Rnw:974-976
 ###################################################
 yrs = 1970:1986
 time = as.POSIXct(paste(yrs, "-01-01", sep=""), tz = "GMT")
 
 
 ###################################################
-### code chunk number 27: jss816.Rnw:984-987
+### code chunk number 27: jss816.Rnw:979-982
 ###################################################
 if (require(plm, quietly = TRUE)) {
  data("Produc")
@@ -286,7 +282,7 @@ if (require(plm, quietly = TRUE)) {
 
 
 ###################################################
-### code chunk number 28: jss816.Rnw:993-996
+### code chunk number 28: jss816.Rnw:988-991
 ###################################################
 if (require(plm, quietly = TRUE))
 # deselect District of Columbia, polygon 8, which is not present in Produc:
@@ -294,14 +290,14 @@ if (require(plm, quietly = TRUE))
 
 
 ###################################################
-### code chunk number 29: jss816.Rnw:998-1000
+### code chunk number 29: jss816.Rnw:993-995
 ###################################################
 if (require(plm, quietly = TRUE) && require(RColorBrewer, quietly = TRUE))
   stplot(Produc.st[,,"unemp"], yrs, col.regions = brewer.pal(9, "YlOrRd"),cuts=9)
 
 
 ###################################################
-### code chunk number 30: jss816.Rnw:1023-1026
+### code chunk number 30: jss816.Rnw:1018-1021
 ###################################################
 if (require(plm, quietly = TRUE))
   zz <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
@@ -309,7 +305,7 @@ if (require(plm, quietly = TRUE))
 
 
 ###################################################
-### code chunk number 31: jss816.Rnw:1047-1054
+### code chunk number 31: jss816.Rnw:1042-1049
 ###################################################
 if (require(gstat, quietly = TRUE)) {
 data("wind")
@@ -321,7 +317,7 @@ proj4string(wind.loc) = "+proj=longlat +datum=WGS84"
 
 
 ###################################################
-### code chunk number 32: jss816.Rnw:1059-1066
+### code chunk number 32: jss816.Rnw:1054-1061
 ###################################################
 if (require(mapdata, quietly = TRUE)) {
 plot(wind.loc, xlim = c(-11,-5.4), ylim = c(51,55.5), axes=T, col="red",
@@ -333,7 +329,7 @@ text(coordinates(wind.loc), pos=1, label=wind.loc$Station, cex=.7)
 
 
 ###################################################
-### code chunk number 33: jss816.Rnw:1078-1081
+### code chunk number 33: jss816.Rnw:1073-1076
 ###################################################
 if (require(gstat, quietly = TRUE)) {
   wind[1:3,]
@@ -341,7 +337,7 @@ if (require(gstat, quietly = TRUE)) {
 
 
 ###################################################
-### code chunk number 34: jss816.Rnw:1085-1089
+### code chunk number 34: jss816.Rnw:1080-1084
 ###################################################
 if (require(gstat, quietly = TRUE)) {
 wind$time = ISOdate(wind$year+1900, wind$month, wind$day)
@@ -350,7 +346,7 @@ wind$jday = as.numeric(format(wind$time, '%j'))
 
 
 ###################################################
-### code chunk number 35: jss816.Rnw:1094-1103
+### code chunk number 35: jss816.Rnw:1089-1098
 ###################################################
 if (require(gstat, quietly = TRUE)) {
 stations = 4:15
@@ -364,7 +360,7 @@ velocities = apply(windsqrt, 2, function(x) { x - meanwind })
 
 
 ###################################################
-### code chunk number 36: jss816.Rnw:1108-1114
+### code chunk number 36: jss816.Rnw:1103-1109
 ###################################################
 if (require(gstat, quietly = TRUE)) {
 wind.loc = wind.loc[match(names(wind[4:15]), wind.loc$Code),]
@@ -375,16 +371,15 @@ pts = SpatialPoints(pts, CRS("+proj=longlat +datum=WGS84 +ellps=WGS84"))
 
 
 ###################################################
-### code chunk number 37: jss816.Rnw:1120-1124
+### code chunk number 37: jss816.Rnw:1115-1118
 ###################################################
-if (require(rgdal, quietly = TRUE)) {
-utm29 = CRS("+proj=utm +zone=29 +datum=WGS84 +ellps=WGS84")
-pts = spTransform(pts, utm29)
-}
+utm29 = "+proj=utm +zone=29 +datum=WGS84 +ellps=WGS84"
+pts.sfc = st_transform(st_as_sfc(pts), utm29)
+pts = as(pts.sfc, "Spatial") # back to sp
 
 
 ###################################################
-### code chunk number 38: jss816.Rnw:1128-1133
+### code chunk number 38: jss816.Rnw:1122-1127
 ###################################################
 if (require(gstat, quietly = TRUE)) {
 wind.data = stConstruct(velocities, space = list(values = 1:ncol(velocities)), 
@@ -394,19 +389,17 @@ class(wind.data)
 
 
 ###################################################
-### code chunk number 39: jss816.Rnw:1136-1143
+### code chunk number 39: jss816.Rnw:1130-1135
 ###################################################
-if (require(maptools, quietly = TRUE) && require(mapdata, quietly = TRUE)) {
-  m = map2SpatialLines(
-	#map("worldHires", xlim = c(-11,-5.4), ylim = c(51,55.5), plot=F))
-	map("worldHires", xlim = c(-11.5,-6.0), ylim = c(51.3,55.0), plot=F))
-  proj4string(m) = "+proj=longlat +datum=WGS84"
-  m = spTransform(m, utm29)
+if (require(sf, quietly = TRUE) && require(mapdata, quietly = TRUE)) {
+  m.sf = st_as_sf(map("worldHires", xlim = c(-11.5,-6.0), ylim = c(51.3,55.0), plot=FALSE), fill = FALSE)
+  m.sf = st_transform(m.sf, utm29)
+  m = as(m.sf, "Spatial")
 }
 
 
 ###################################################
-### code chunk number 40: jss816.Rnw:1146-1149
+### code chunk number 40: jss816.Rnw:1138-1141
 ###################################################
 if (require(gstat, quietly = TRUE))
   grd = SpatialPixels(SpatialPoints(makegrid(m, n = 300)),
@@ -414,14 +407,14 @@ if (require(gstat, quietly = TRUE))
 
 
 ###################################################
-### code chunk number 41: jss816.Rnw:1152-1154
+### code chunk number 41: jss816.Rnw:1144-1146
 ###################################################
 if (require(gstat, quietly = TRUE))
   wind.data = wind.data[, "1961-04"]
 
 
 ###################################################
-### code chunk number 42: jss816.Rnw:1158-1164
+### code chunk number 42: jss816.Rnw:1150-1156
 ###################################################
 if (require(gstat, quietly = TRUE)) {
 n = 10
@@ -432,7 +425,7 @@ pred.grd = STF(grd, tgrd)
 
 
 ###################################################
-### code chunk number 43: jss816.Rnw:1168-1174
+### code chunk number 43: jss816.Rnw:1160-1166
 ###################################################
 if (require(gstat, quietly = TRUE)) {
 v = vgmST("separable", space = vgm(1, "Exp", 750000), time = vgm(1, "Exp", 1.5 * 3600 * 24),
@@ -443,7 +436,7 @@ colnames(wind.ST@data) <- "sqrt_speed"
 
 
 ###################################################
-### code chunk number 44: jss816.Rnw:1180-1187 (eval = FALSE)
+### code chunk number 44: jss816.Rnw:1172-1179 (eval = FALSE)
 ###################################################
 ## if (require(gstat, quietly = TRUE)) {
 ## layout = list(list("sp.lines", m, col='grey'),
@@ -455,7 +448,7 @@ colnames(wind.ST@data) <- "sqrt_speed"
 
 
 ###################################################
-### code chunk number 45: jss816.Rnw:1190-1200
+### code chunk number 45: jss816.Rnw:1182-1192
 ###################################################
 pdf("wind.pdf", height=4.5)
 if (require(gstat, quietly = TRUE)) {
@@ -470,7 +463,7 @@ dev.off()
 
 
 ###################################################
-### code chunk number 46: jss816.Rnw:1203-1217
+### code chunk number 46: jss816.Rnw:1195-1209
 ###################################################
 pdf("windts.pdf", height = 4)
 if (require(gstat, quietly = TRUE)) {
@@ -489,7 +482,7 @@ dev.off()
 
 
 ###################################################
-### code chunk number 47: jss816.Rnw:1220-1227 (eval = FALSE)
+### code chunk number 47: jss816.Rnw:1212-1219 (eval = FALSE)
 ###################################################
 ## if (require(gstat, quietly = TRUE)) {
 ## pdf("hov.pdf")
@@ -501,28 +494,28 @@ dev.off()
 
 
 ###################################################
-### code chunk number 48: jss816.Rnw:1233-1235 (eval = FALSE)
+### code chunk number 48: jss816.Rnw:1225-1227 (eval = FALSE)
 ###################################################
 ## if (require(gstat, quietly = TRUE))
 ##   eof.data = eof(wind.data)
 
 
 ###################################################
-### code chunk number 49: jss816.Rnw:1238-1240
+### code chunk number 49: jss816.Rnw:1230-1232
 ###################################################
 if (require(gstat, quietly = TRUE))
   eof.int = eof(wind.ST)
 
 
 ###################################################
-### code chunk number 50: jss816.Rnw:1244-1246 (eval = FALSE)
+### code chunk number 50: jss816.Rnw:1236-1238 (eval = FALSE)
 ###################################################
 ## if (require(gstat, quietly = TRUE))
 ##   eof.xts = eof(wind.ST, "temporal")
 
 
 ###################################################
-### code chunk number 51: jss816.Rnw:1255-1261
+### code chunk number 51: jss816.Rnw:1247-1253
 ###################################################
 if (require(gstat, quietly = TRUE)) {
   print(spplot(eof.int[1:4], col.regions=bpy.colors(),
@@ -533,7 +526,7 @@ if (require(gstat, quietly = TRUE)) {
 
 
 ###################################################
-### code chunk number 52: jss816.Rnw:1278-1289
+### code chunk number 52: jss816.Rnw:1270-1281
 ###################################################
 if (require(adehabitatLT, quietly = TRUE)) {
 data("puechabonsp")
@@ -549,14 +542,14 @@ l2
 
 
 ###################################################
-### code chunk number 53: jss816.Rnw:1293-1295 (eval = FALSE)
+### code chunk number 53: jss816.Rnw:1285-1287 (eval = FALSE)
 ###################################################
 ## sttdf = as(l2, "STTDF")
 ## stplot(sttdf, by="time*id")
 
 
 ###################################################
-### code chunk number 54: jss816.Rnw:1301-1306
+### code chunk number 54: jss816.Rnw:1293-1298
 ###################################################
 if (require(adehabitatLT, quietly = TRUE)) {
 sttdf = as(l2, "STTDF")
@@ -566,7 +559,7 @@ print(stplot(sttdf, by="time*id"))
 
 
 ###################################################
-### code chunk number 55: jss816.Rnw:1319-1324
+### code chunk number 55: jss816.Rnw:1311-1316
 ###################################################
 if (require(cshapes, quietly = TRUE)) {
   library(sf)
@@ -576,7 +569,7 @@ if (require(cshapes, quietly = TRUE)) {
 
 
 ###################################################
-### code chunk number 56: jss816.Rnw:1335-1338
+### code chunk number 56: jss816.Rnw:1327-1330
 ###################################################
 if (require(cshapes, quietly = TRUE))
   st = STIDF(geometry(as(cs, "Spatial")), 
@@ -584,7 +577,7 @@ if (require(cshapes, quietly = TRUE))
 
 
 ###################################################
-### code chunk number 57: jss816.Rnw:1342-1346
+### code chunk number 57: jss816.Rnw:1334-1338
 ###################################################
 if (require(cshapes, quietly = TRUE)) {
 pt = SpatialPoints(cbind(7, 52), CRS(proj4string(st)))
@@ -593,14 +586,14 @@ as.data.frame(st[pt,,1:5])
 
 
 ###################################################
-### code chunk number 58: jss816.Rnw:1367-1369 (eval = FALSE)
+### code chunk number 58: jss816.Rnw:1359-1361 (eval = FALSE)
 ###################################################
 ## library(spacetime)
 ## demo(CressieWikle)
 
 
 ###################################################
-### code chunk number 59: jss816.Rnw:1380-1383 (eval = FALSE)
+### code chunk number 59: jss816.Rnw:1372-1375 (eval = FALSE)
 ###################################################
 ## if (require(gstat, quietly = TRUE)) {
 ##   vignette("st")
@@ -608,14 +601,14 @@ as.data.frame(st[pt,,1:5])
 
 
 ###################################################
-### code chunk number 60: jss816.Rnw:1389-1391 (eval = FALSE)
+### code chunk number 60: jss816.Rnw:1381-1383 (eval = FALSE)
 ###################################################
 ## library(spacetime)
 ## vignette("stpg")
 
 
 ###################################################
-### code chunk number 61: jss816.Rnw:1403-1405 (eval = FALSE)
+### code chunk number 61: jss816.Rnw:1395-1397 (eval = FALSE)
 ###################################################
 ## library(spacetime)
 ## vignette("sto")
